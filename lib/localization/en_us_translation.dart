bb4482083eacafcf3d027b183199b8cd9377e3a5
öznur timur <oznur.timur@ozu.edu.tr>
2023-11-17 11:45:33 +0100

    translation file is implemented

---
+++ lib/localization/en_us_translation.dart
@@ -0,0 +1,13 @@
+final Map<String, String> enUs = {
+  // Home Screen
+  "lbl_125": "125", "lbl_1_hour_ago": "1 hour ago", "lbl_20": "20",
+  "lbl_bruno": "Bruno", "lbl_thanh_pham": "Thanh Pham",
+
+  // Home - Tab Container Screen
+  "lbl_following": "Following", "lbl_popular": "Popular",
+  "lbl_search": "Search ", "lbl_trending": "Trending",
+
+// Network Error String
+  "msg_network_err": "Network Error",
+  "msg_something_went_wrong": "Something Went Wrong!",
+};
