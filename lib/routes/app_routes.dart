eaaf7f1c09600e8cb805946b5f6c01f4e721b799
öznur timur <oznur.timur@ozu.edu.tr>
2023-11-21 18:13:20 +0100

    routes are defined

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:dmood/views/home_container_screen.dart';
+import 'package:dmood/views/app_navigation_screen.dart';
+
+class AppRoutes {
+  static const String homePage = '/home_page';
+
+  static const String homeTabContainerPage = '/home_tab_container_page';
+
+  static const String homeContainerScreen = '/home_container_screen';
+
+  static const String appNavigationScreen = '/app_navigation_screen';
+
+  static Map<String, WidgetBuilder> routes = {
+    homeContainerScreen: (context) => HomeContainerScreen(),
+    appNavigationScreen: (context) => AppNavigationScreen()
+  };
+}
